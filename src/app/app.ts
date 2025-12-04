4a711d29188ccdbe81fefcc128e1b2ac4037ac23
Safat <safatrangrezzz@gmail.com>
2025-12-05 00:32:59 +0530

    project in process

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,55 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Aslam-fireworks');
+
+
+
+  display:any[] =[
+
+
+    {name: 'Mori Hanimoon 12 creakling shot',
+      brand: 'Mori',
+      price: '450',
+      img: 'Mori Honeymoon.jpg'
+    },
+  
+    {name: 'Mori Color Koti Pots',
+      brand: 'Mori',
+      price: '400',
+      img: 'Mori Color koti.jpg'
+    },
+  
+    {name: 'Vanitha IQ Out Skyshot',
+      brand: 'Vanitha',
+      price: '1300',
+      img: 'iq-out.png'
+    },
+    {name: 'Mori Lagan Munch',
+      brand: 'Mori',
+      price: '1900',
+      img: 'Mori lagan munch.jpg'
+    },
+    {name: 'Mori Silver Rain',
+      brand: 'Mori',
+      price: '1900',
+      img: 'Mori silver rain.jpg'
+    },
+    {name: 'Mori Penta Plus Mini skyshot',
+      brand: 'Mori',
+      price: '1900',
+      img: 'Mori penta.jpg'
+    },
+    {name: 'Mori Mojto Creakling Pots',
+      brand: 'Mori',
+      price: '1900',
+      img: 'mori-mojito-3-pcs.jpg'
+    },
+  ]
 }
