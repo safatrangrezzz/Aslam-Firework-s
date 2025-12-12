ea47b74165b1a7084168d45a7db9b730acc42e2e
Safat <safatrangrezzz@gmail.com>
2025-12-12 15:53:03 +0530

    whtsapp-Add"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { About } from './about/about';
 import { Products } from './products/products';
 import { SafetyGuide } from './safety-guide/safety-guide';
 import { SubLandingPage } from './sub-landing-page/sub-landing-page';
+import { DataEditComponent } from './data-edit-component/data-edit-component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: SubLandingPage},
     {path: 'about', component: About},
     {path: 'products', component: Products},
-    {path: 'safety', component:SafetyGuide}
+    {path: 'safety', component:SafetyGuide},
+    {path: 'setting', component:DataEditComponent}
 ];
---
+++ src/app/data-edit-component/data-edit-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-edit-component',
+  imports: [],
+  templateUrl: './data-edit-component.html',
+  styleUrl: './data-edit-component.scss',
+})
+export class DataEditComponent {
+
+}
